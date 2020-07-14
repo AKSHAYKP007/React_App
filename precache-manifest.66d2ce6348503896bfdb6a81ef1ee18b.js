77f4ad86035c24e3e6ba6c78e9a176cd4809b1b2
akshaykp007 <akshaykp2661998@gmail.com>
2020-07-14 14:55:03 +0100

    Updates

---
+++ precache-manifest.66d2ce6348503896bfdb6a81ef1ee18b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aceea5b9b25f19cc09ac0ca33b7c770",
+    "url": "/React_App/index.html"
+  },
+  {
+    "revision": "8fe66cb24eb69e2940a7",
+    "url": "/React_App/static/css/main.29907e28.chunk.css"
+  },
+  {
+    "revision": "077f42be16ecb0c4eb7d",
+    "url": "/React_App/static/js/2.459aa882.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React_App/static/js/2.459aa882.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8fe66cb24eb69e2940a7",
+    "url": "/React_App/static/js/main.a0e84061.chunk.js"
+  },
+  {
+    "revision": "4379c441828c0f30b7e5",
+    "url": "/React_App/static/js/runtime-main.e97c4d8b.js"
+  }
+]);
